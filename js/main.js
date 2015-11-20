a4a99ec1ab3e7bc6f1871438f097a5e5fa28d6b5
AleksandarP91 <Pejcinovski91@gmail.com>
2015-11-20 20:31:53 +0100

    Project uppload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+var myImage = document.getElementById("img");
+
+var imageArray= ["images/bg2.png","images/bg3.png","images/bg5.png" ];
+
+var imageIndex = 0;
+
+function imageSlider(){
+
+        myImage.setAttribute("src",imageArray[imageIndex]);
+        imageIndex++;
+        if (imageIndex >= imageArray.length) {
+        	imageIndex=0;
+        }
+
+}
+setInterval(imageSlider,5000);
+
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null || x == "") {
+        document.getElementById("return").innerHTML = "Please enter a username";
+        return false;
+    }
+}
+
+
